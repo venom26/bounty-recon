e54adfffd7eb8205b349d48f2a31340142e37c77
venom26 <54362015+venom26@users.noreply.github.com>
2021-06-22 13:12:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ pip install -r requirements.txt
 sudo python setup.py install
 cd ~/tools/
 echo "done"
-
+sudo apt-get install xdg-utils
 cd ~/tools
 echo "installing massdns"
 git clone https://github.com/blechschmidt/massdns.git
@@ -22,6 +22,7 @@ echo "done"
 git clone https://github.com/ebsa491/whichCDN.git
 cd whichCDN
 pip install -r requirements.txt
+sudo chmod +x whichCDN
 cd ~/tools
 
 wget wget https://github.com/epi052/feroxbuster/releases/download/v2.3.0/x86_64-linux-feroxbuster.zip
@@ -36,12 +37,20 @@ sudo chmod +x install.sh
 ./install.sh
 cd ~/tools
 
+git clone https://github.com/theZiz/aha.git
+cd aha
+make
+sudo cp aha /usr/bin/
+cd ~/tools
+
 git clone https://github.com/yassineaboukir/Asnlookup && cd Asnlookup
 pip install -r requirements.txt
 pip3 install -r requirements.txt
 cd ~/tools
 
 git clone https://github.com/sathishshan/Zone-transfer.git
+cd Zone-transfer
+sudo chmod +x zone-t.sh
 cd ~/tools
 
 echo "Installing Aquatone"
@@ -51,6 +60,6 @@ unzip aquatone_linux_amd64_1.7.0.zip
 sudo mv aquatone /usr/bin/
 rm -rf aquatone_linux_amd64_1.7.0.zip README.md LICENSE.txt
 echo "done"
-
+go get github.com/nytr0gen/deduplicate
 cd ~/tools
 git clone https://github.com/danielmiessler/SecLists.git
